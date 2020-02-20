f3df800499d1e5ba45a65c5c87c0424388769e62
Jungbey <jacbeywan@gmail.com>
2020-02-20 18:37:43 +0800

    修改接口设置

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,9 +1,9 @@
 const proxy = require('http-proxy-middleware')
 
 module.exports = function (app) {
-    app.use(proxy('/code', {
-        target: 'https://www.chsi.com.cn/cet/ValidatorIMG.JPG?ID=3341.068026493503',
-        secure: true,
+    app.use(proxy('/api', {
+        target: 'http://47.103.220.76:80/api/',
+        secure: false,
         changeOrigin: true,
         pathRewrite: {
             "^/rjwl": "/rjwl"
